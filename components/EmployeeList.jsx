63677c4424e6da37ebfd44a362dc42ac23d2740a
Krystle Capuano <krystlecapuano@gmail.com>
2024-06-07 14:49:23 -0400

    Files updated

---
+++ components/EmployeeList.jsx
@@ -0,0 +1,17 @@
+// function PerScholasLogo() {
+//   const imgURL = "https://perscholas.org/wp-content/themes/per-scholas/assets/images/logo1.svg"
+//   const alt = "Per Scholas"
+
+//   return (
+//     <div>
+      
+//       <img 
+//         src={imgURL}
+//         className='logo'
+//         alt={alt}
+//       />
+//     </div>
+//   )
+// }
+
+// export default PerScholasLogo
